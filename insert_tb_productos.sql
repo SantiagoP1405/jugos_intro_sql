0f3da2b5cabc5bec494a7a9123c6edbe7cb0f73a
SantiagoP1405 <116893930+SantiagoP1405@users.noreply.github.com>
2025-04-14 10:45:25 -0600

    INSERT INTO tb_productos
    Primeros datos insertados en la tabla 'tb_productos'

---
+++ insert_tb_productos.sql
@@ -0,0 +1,6 @@
+USE JUGOS;
+INSERT INTO tb_productos(
+producto, nombre, envase, volumen, sabor, precio
+) VALUES ('773912', 'clean', 'botella PET', '1 litro', 'naranja', 8.01);
+
+SELECT * FROM tb_productos
