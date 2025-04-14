417d8f737f430476c5dc86b1acbe6f354cb78896
SantiagoP1405 <116893930+SantiagoP1405@users.noreply.github.com>
2025-04-14 10:50:21 -0600

    INSERT de múltiples datos en tb_productos
    Se insertaron 3 sets de datos en la tabla de 'tb_productos'

--- insert_tb_productos.sql
+++ insert_tb_productos.sql
@@ -3,4 +3,16 @@ INSERT INTO tb_productos(
 producto, nombre, envase, volumen, sabor, precio
 ) VALUES ('773912', 'clean', 'botella PET', '1 litro', 'naranja', 8.01);
 
+INSERT INTO tb_productos(
+producto, nombre, envase, volumen, sabor, precio
+) VALUES ('838819', 'clean', 'botella PET', '1.5 litros', 'naranja', 12.01);
+
+INSERT INTO tb_productos(
+producto, nombre, envase, volumen, sabor, precio
+) VALUES ('1037797', 'clean', 'botella PET', '2 litros', 'naranja', 16.01);
+
+INSERT INTO tb_productos(
+producto, nombre, envase, volumen, sabor, precio
+) VALUES ('812829', 'clean', 'lata', '350 ml', 'naranja', 2.81);
+
 SELECT * FROM tb_productos
