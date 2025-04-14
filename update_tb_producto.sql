3e2983a173f101e2dde9a45d005e10e439ef2391
SantiagoP1405 <116893930+SantiagoP1405@users.noreply.github.com>
2025-04-14 11:14:21 -0600

    UPDATE en 'tb_productos'
    Se corrigieron algunas entradas en las columnas de la tabla 'tb_productos'

---
+++ update_tb_producto.sql
@@ -0,0 +1,17 @@
+USE jugos;
+
+INSERT INTO tb_productos(
+producto, nombre, envase, volumen, sabor,
+precio) VALUES ('695594', 'Festival de Sabores', 'Botella PET',
+'1.5 Litros', 'Asaí', 18.51);
+
+INSERT INTO tb_productos(
+producto, nombre, envase, volumen, sabor,
+precio) VALUES ('1041119', 'Línea Citrus', 'Botella de Vidrio',
+'700 ml', 'Lima', 4.90);
+
+SELECT * FROM tb_productos;
+
+UPDATE tb_productos SET PRECIO = 28.51 WHERE PRODUCTO = '695594';
+UPDATE tb_productos SET SABOR = 'Lima/Limón' WHERE PRODUCTO = '1041119';
+
